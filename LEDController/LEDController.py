ba7e2c01a88473d6e689edb81f5e678835119bb4
Amy Harrison <amy.harrison@capita.co.uk>
2020-01-03 11:07:39 +0000

    Housekeeping and adding things into functions, layed foundations for bluetooth connectivity

--- LEDController/LEDController.py
+++ LEDController/LEDController.py
@@ -2,88 +2,152 @@
 import numpy as np
 import cv2 as cv 
 
-devIn = ""
 
-try:
-	import RPI.GPIO as gpio
-	#really dumb
-	gpioActive = True
-	#set pins
-	rCh = 20
-	bCh = 21
-	gCh = 22
-	#set mode
-	gpio.setup(rCh,gpio.OUT)
-	gpio.setup(gCh,gpio.OUT)
-	gpio.setup(bCh,gpio.OUT)
-	#set frequency of pwm
-	pwmFreq = 255
-	#define pwm for pins
-	redCh   = gpio.PWM(rCh, pwmFreq)
-	greenCh = gpio.PWM(gCh, pwmFreq)
-	blueCh  = gpio.PWM(bCh, pwmFreq)
-	
-except:
-	print('could not find gpio library')
-	gpioActive = False
+#initialise the device list 
+strDevIn = ""
+#-----------------------------------------------------------------------
+#      	bluetooth setup v0.2
+# 		initialises bluetooth while checking if devices are accessable
+#		started: 17/12/2019 updated: 03/01/2020
+#		Author: AH
+#-----------------------------------------------------------------------
+def BluetoothSetup(strDevIn):
+	try:
+		import pybluez
+		strstrDevIn += " BLUETOOTH"
+	except:
+		print("bluetooth library not found")
+	return strDevIn
 
-try:
-	import pyaudio
-	from sense_hat import SenseHat
-	sh = SenseHat()
-	sh.clear
-	hat = True
-	gpioActive = False
-	r,g,b = 0,0,0
-	backLight = np.array([
-	[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-	[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-	[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-	[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-	[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-	[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-	[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-	[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b]])
-		
-except:
-	print('could not find senseHat library')
-	hat = False
+#-----------------------------------------------------------------------
+#      	audio setup v0.2
+# 		initialises the audio while checking if audio is accessable
+#		started: 17/12/2019 updated: 03/01/2020
+#		Author: AH
+#-----------------------------------------------------------------------
+def AudioSetup(strDevIn):
+	try:
+		import pyaudio
+		CHUNK = 1024
+		FORMAT = pyaudio.paInt16
+		CHANNELS = 2
+		RATE = 44100
 
-#change image to capture card location
-cam = cv.VideoCapture(1)
+		p = pyaudio.PyAudio()
 
-#read image for framer size and create blank for troubleshooting
-ret, frame = cam.read()
-if ret == True:
-	devIn += "CAMERA"
-b,g,r = cv.split(frame)
-blank = b - b
+		stream = p.open(format=FORMAT,
+			channels=CHANNELS,
+			rate=RATE,
+			input=True,
+			frames_per_buffer=CHUNK)
 
-#audio properties
-intAudioRange = 20000 # 20KHz
-intAlias = int(16777215/intAudioRange)
+		strDevIn += " AUDIO"
+	except:
+		print("no audio device")
+	return strDevIn
 
-try:
-	CHUNK = 1024
-	FORMAT = pyaudio.paInt16
-	CHANNELS = 2
-	RATE = 44100
+#-----------------------------------------------------------------------
+#      	GPIO setup v0.2
+# 		initialises the GPIO while checking if GPIO are accessable
+#		started: 17/12/2019 updated: 03/01/2020
+#		Author: AH
+#-----------------------------------------------------------------------
+def GPIOSetup():
+	try:
+		import RPI.GPIO as gpio
+		strDevIn += " GPIO"
+		#set pins
+		rCh = 20
+		bCh = 21
+		gCh = 22
+		#set mode
+		gpio.setup(rCh,gpio.OUT)
+		gpio.setup(gCh,gpio.OUT)
+		gpio.setup(bCh,gpio.OUT)
+		#set frequency of pwm
+		pwmFreq = 255
+		#define pwm for pins
+		redCh   = gpio.PWM(rCh, pwmFreq)
+		greenCh = gpio.PWM(gCh, pwmFreq)
+		blueCh  = gpio.PWM(bCh, pwmFreq)
+	except:
+		print('could not find gpio library')
+	return strDevIn
 
-	p = pyaudio.PyAudio()
+#-----------------------------------------------------------------------
+#      	sense Hat setup v0.2
+# 		initialises the sense hat while checking if 
+# 		devices are accessable
+#		started: 17/12/2019 updated: 03/01/2020
+#		Author: AH
+#-----------------------------------------------------------------------
+def SenseHatSetup(strDevIn)
+	try:
+		from sense_hat import SenseHat
+		sh = SenseHat()
+		sh.clear
+		strDevIn += " SENSE-HAT"
+		gpioActive = False
+		r,g,b = 0,0,0
+		backLight = np.array([
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b]])
+	except:
+		print('could not find senseHat library')
+	return strDevIn
+#-----------------------------------------------------------------------
+#      	Camera setup v0.1
+# 		initialises camera and checks for connection 
+#		started: 03/01/2020 updated: 
+#		Author: AH
+#-----------------------------------------------------------------------
+def CameraSetup(strDevIn):
+	cam = cv.VideoCapture(0)
+	ret, frame = cam.read()
+	if ret == True:
+		strDevIn += " CAMERA"
+	return strDevIn
 
-	stream = p.open(format=FORMAT,
-		channels=CHANNELS,
-		rate=RATE,
-		input=True,
-		frames_per_buffer=CHUNK)
+#-----------------------------------------------------------------------
+#      	setup v0.1
+# 		initialises all
+#		started: 03/01/2020 updated: 
+#		Author: AH
+#-----------------------------------------------------------------------
+def Setup(strDevIn):
+	strDevIn = BluetoothSetup(strDevIn)
+	strDevIn = AudioSetup(strDevIn)
+	strDevIn = GPIOSetup(strDevIn)
+	strDevIn = SenseHatSetup(strDevIn)
+	strDevIn = CameraSetup(strDevIn)
 
-	devIn += "AUDIO"
+	return strDevIn
 
-except:
-	print("no audio device")
+#frame properties
+if "CAMERA" in devIn:
+	ret, frame = cam.read()
+	b,g,r = cv.split(frame)
+	blank = b - b
 
+#audio properties 20 Hz - 20KHz to Hex
+if "AUDIO" in devIn:
+	intAudioRange = 20000 - 20
+	intAlias = int(16777215/intAudioRange)
+
+#-----------------------------------------------------------------------
+#      	Main v0.1
+# 		Main body for control software
+#		started: 17/12/2019 updated: 03/01/2020
+#		Author: AH
+#-----------------------------------------------------------------------
 while True:
-	if "CAMERA" in devIn:
+	if "CAMERA" in strDevIn:
 		#read frame and split colour channels
 		ret, frame = cam.read()
 		imgBlue,imgGreen,imgRed = cv.split(frame)
@@ -93,7 +157,7 @@ while True:
 		g = int(np.mean(imgGreen))
 		r = int(np.mean(imgRed))
 
-	if "AUDIO" in devIn:
+	if "AUDIO" in strDevIn:
 		#read audio device
 		inAaudio = stream.read(CHUNK)
 
@@ -102,8 +166,8 @@ while True:
 		h = input('Enter hex: ').lstrip('#')
 		R, G, B = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
 	
-	#output average to LED controller
-	if gpioActive == True:
+
+	if "GPIO" in strDevIn:
 		#pwm for analogue output
 		redCh.start(100)
 		greenCh.start(1)
@@ -114,25 +178,28 @@ while True:
 		greenCh.ChangeDutyCycle(g)
 		blueCh.ChangeDutyCycle(b)
 	
-	if hat == True:
-		sh.set_pixels(backLight)
+	if "BLUETOOTH" in strDevIn:
+		#do bluetooth stuff plz
 	
-	backLight = np.array([
-		[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-		[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-		[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-		[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-		[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-		[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-		[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
-		[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b]])
-		
+	if "SENSE-HAT" in strDevIn:
+		#output colour to sense hat LED matrix 
+		sh.set_pixels(backLight)
+		backLight = np.array([
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],
+			[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b],[r,g,b]])
+	
 	try:
 		#troubleshooting
 		backlight = cv.merge((blank+b, blank+g, blank+r))
-		cv.imshow('img', frame)
+		cv.imshow('image frame', frame)
 		cv.imshow('backlight', backlight)
 	except:
 		print("Cannot show troubleshooting")
-
+	#adjust for higher/lower bitrates 
 	cv.waitKey(1)
